3c3ec4591071ce3978e321fff38095f7384a7c74
lucasrgris <lucasrgris@gmail.com>
2018-08-13 13:11:17 -0300

    Update convert module to use sox software

--- util/towav.py
+++ util/towav.py
@@ -1,27 +1,55 @@
 """
 towav: convert audio files to wav format
 
-This module uses the mpg123 software to convert audio files to wav format.
-Please be sure you have this software installed in your PATH system variable.
-More details in https://www.mpg123.de/index.shtml.
+Dependencies:
+- mpg123: this module uses the mpg123 software to convert audio files to wav
+format. Please be sure you have this software installed in your PATH system
+variable. More details in https://www.mpg123.de/index.shtml.
+- sox: this module uses the mpg123 software to convert audio files to wav
+format. Please be sure you have this software installed in your PATH system
+variable. More details in
 """
 
 import os
 import argparse
 
 
-def convert(input_path: str, output_path: str):
+def mpg_convert(input_path: str, output_path: str, check=True):
     """
     Convert an audio file to wav format.
 
-    :param input_path: input file path
-    :param output_path: output file path
+    :param input_path: str
+        Input file path
+    :param output_path: str
+        Input file path
+    :param check: bool
+        Check if the output file exists
 
     Currently supported formats: formats supported by mpg123 software.
     """
     os.system('mpg123 -w ' + output_path + ' ' + input_path)
-    if not os.path.isfile(output_path):
-        raise RuntimeError('Not able to convert file')
+    if check:
+        if not os.path.isfile(output_path):
+            raise RuntimeError('Not able to convert file')
+
+
+def sox_convert(input_path: str, output_path: str, check=True):
+    """
+    Convert an audio file to wav format.
+
+    :param input_path: str
+        Input file path
+    :param output_path: str
+        Input file path
+    :param check: bool
+        Check if the output file exists
+
+    Currently supported formats: formats supported by sox software.
+    """
+    os.system('sox ' + input_path + ' ' + output_path)
+    if check:
+        if not os.path.isfile(output_path):
+            raise RuntimeError('Not able to convert file')
 
 
 if __name__ == '__main__':
@@ -36,6 +64,6 @@ if __name__ == '__main__':
 
     file_names = os.listdir(args.data_dir)
     for i, line in enumerate(file_names):
-        convert(args.data_dir + '/' + line,
-                args.out_dir + "/" + line[:-4] + '.wav')
+        mpg_convert(args.data_dir + '/' + line,
+                    args.out_dir + "/" + str(line[:-4]) + '.wav')
         print("Processed {} of {} files".format(i, len(file_names)))
