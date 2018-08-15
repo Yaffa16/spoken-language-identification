dbb2b16d8a106e5559c953915731cc82003c0a3a
lucasrgris <lucasrgris@gmail.com>
2018-08-15 12:39:22 -0300

    Add documentation and log failed processed files feature + updates

--- script_specs.py
+++ script_specs.py
@@ -1,5 +1,6 @@
-# TODO: Documentation
-# TODO: Log failed processed files
+"""
+This script generates spectrogram of wav audio files using parallel processing.
+"""
 
 from preprocessing.spectrogram import *
 from tqdm import tqdm
@@ -18,7 +19,8 @@ if not sys.warnoptions:
 def files_to_process(files_list, output_dir):
     this_files = files_list.copy()
     print('Checking files in ', output_dir)
-    specs_imgs = 0
+    counter = 0
+    # List the output files in the directory
     output_files = os.listdir(output_dir)
     for file_name in files_list:
         if str(file_name[:-3]) + 'png' in output_files:
@@ -26,25 +28,47 @@ def files_to_process(files_list, output_dir):
             specs_imgs += 1
     print('There are {} files remaining to process, '
           'and {} spectrogram images in {}.'.format(len(files_list)
-                                                    - specs_imgs,
-                                                    specs_imgs,
+                                                    - counter,
+                                                    counter,
                                                     output_dir))
     return this_files
 
 
-def process_files_in_parallel(fun: callable, data_path: str, plot_path: str,
-                              files_list: list, **kwargs):
+def process_files_in_parallel(data_path: str, plot_path: str, files_list: list,
+                              fun: callable, workers=None, **kwargs):
+    """
+    Process files in parallel, calling the provided function.
+
+    Expected function signature: audiopath: str, plotpath: str, name: str,
+        **kwargs
+
+    ..see: preprocessing.spectrogram for more details.
+
+    :param data_path: str
+        Source of audio files.
+    :param plot_path: str
+        Output directory for plot images.
+    :param files_list: list
+        List of files to process.
+    :param fun:
+        A function to call. Default to preprocessing.spectrogram function.
+    :param workers: int
+        The maximum number of processes that can be used to execute the given
+        calls
+    :param kwargs:
+        Additional kwargs are passed on to the callable object.
+    """
     if len(files_list) == 0:
         return
     else:
         print('Processing files through function', fun.__name__, kwargs)
-    with concurrent.futures.ProcessPoolExecutor(arguments.workers) \
+    with concurrent.futures.ProcessPoolExecutor(workers) \
             as executor:
-        futures = [executor.submit(fun, audiopath=data_path + '/' + file_p,
+        futures = [executor.submit(fun, audiopath=data_path + os.sep + file_n,
                                    plotpath=plot_path,
-                                   name=file_p[:-4],
+                                   name=file_n[:-4],
                                    **kwargs)
-                   for file_p in files_list]
+                   for file_n in files_list]
 
         kwargs = {
             'total': len(futures),
@@ -54,11 +78,13 @@ def process_files_in_parallel(fun: callable, data_path: str, plot_path: str,
         }
         for f in tqdm(concurrent.futures.as_completed(futures), **kwargs):
             pass
-        with open('logs/script_specs-exceptions.txt', 'a') as file:
-            file.write('Exceptions for {} call at {}'.format(fun.__name__,
-                                                             time.time()))
+        with open('logs/scripts/script_specs-exceptions.txt', 'a') as log:
+            log.write('Exceptions for {} call at {}'.format(fun.__name__,
+                                                            time.time()))
             for f in futures:
-                file.write('\n' + str(f.exception()))
+                if f.exception() is not None:
+                    log.write('\n' + str(f.exception()) +
+                              ' \n\twhen processing ' + f)
 
 
 if __name__ == '__main__':
@@ -67,11 +93,16 @@ if __name__ == '__main__':
                                                  'of wav audio files.')
     parser.add_argument('source', help='Source directory.')
     parser.add_argument('output', help='Output directory.')
-    parser.add_argument('--data', help='Define how many process to run in '
+    parser.add_argument('--workers', help='Define how many process to run in '
                                           'parallel.', default=4, type=int)
     parser.add_argument('--check', help='Check output directories ignoring '
                                         'files already processed.',
                         action='store_true')
+    parser.add_argument('--check_after', help='Check output directories '
+                                              'after processing files and save '
+                                              'a list of failed files.',
+                        action='store_true')
+    os.makedirs('logs/scripts/', exist_ok=True)
 
     # Set source and output directories:
     arguments = parser.parse_args()
@@ -92,7 +123,7 @@ if __name__ == '__main__':
 
     # Check processed files to ignore them
     if arguments.check:
-        print("This might take a while... ")
+        print("Checking, this might take a while... ")
         files_dft = files_to_process(files, output + '/dft')
         files_sox = files_to_process(files, output + '/sox')
         files_lbr_dft = files_to_process(files, output + '/librosa/default')
@@ -113,26 +144,34 @@ if __name__ == '__main__':
 
     with Timer() as timer:
         process_files_in_parallel(fun=specgram_dft, files_list=files_dft,
-                                  data_path=data_dir, plot_path=output + '/dft')
+                                  data_path=data_dir, plot_path=output + '/dft',
+                                  workers=arguments.workers)
         process_files_in_parallel(fun=specgram_sox, files_list=files_sox,
-                                  data_path=data_dir, plot_path=output + '/sox')
+                                  data_path=data_dir, plot_path=output + '/sox',
+                                  workers=arguments.workers)
         process_files_in_parallel(fun=specgram_lbrs, files_list=files_lbr_dft,
                                   data_path=data_dir,
-                                  plot_path=output + '/librosa/default')
+                                  plot_path=output + '/librosa/default',
+                                  workers=arguments.workers)
         process_files_in_parallel(fun=specgram_lbrs, files_list=files_lbr_log,
                                   data_path=data_dir, plot_path=output +
-                                  '/librosa/log', algorithm='log')
+                                  '/librosa/log', algorithm='log',
+                                  workers=arguments.workers)
         process_files_in_parallel(fun=specgram_lbrs, files_list=files_lbr_mel,
                                   data_path=data_dir, plot_path=output +
-                                  '/librosa/mel', algorithm='mel')
+                                  '/librosa/mel', algorithm='mel',
+                                  workers=arguments.workers)
         process_files_in_parallel(fun=specgram_lbrs, files_list=files_lbr_dl,
                                   data_path=data_dir, plot_path=output +
-                                  '/librosa/default/log', y_axis='log')
+                                  '/librosa/default/log', y_axis='log',
+                                  workers=arguments.workers)
         process_files_in_parallel(fun=specgram_lbrs, files_list=files_lbr_dm,
                                   data_path=data_dir, plot_path=output +
-                                  '/librosa/default/mel', y_axis='mel')
+                                  '/librosa/default/mel', y_axis='mel',
+                                  workers=arguments.workers)
         process_files_in_parallel(fun=specgram_mtplt, files_list=files_plt,
                                   data_path=data_dir,
-                                  plot_path=output + '/matplotlib')
+                                  plot_path=output + '/matplotlib',
+                                  workers=arguments.workers)
 
     print('WORK DONE, total taken time: %.03f sec.' % timer.interval)
