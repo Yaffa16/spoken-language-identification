9b0ad28a813489238336fd75c6de5985b8186482
lucasrgris <lucasrgris@gmail.com>
2018-11-03 08:24:51 -0300

    Add features: min rate and data augmentation

--- script_create_dataset.py
+++ script_create_dataset.py
@@ -5,20 +5,21 @@ Dependencies:
     - sox: this module uses the sox software to convert audio files to wav
     format. Please be sure you have this software installed in your PATH system
     variable. More details in http://sox.sourceforge.net
+    - FFmpeg: this module uses FFmpeg for audio normalization. Please be sure
+    you have this software installed in your PATH system variable. More details
+    in http://www.ffmpeg.org/
 
 Note:
     Duplicate files are being ignored.
 """
-import glob
 import concurrent.futures
 import os
 import time
-import json
-import argparse
-import importlib
-from collections import defaultdict
+import sys
+import subprocess
 from tqdm import tqdm
 from shutil import copyfile
+from pydub import AudioSegment
 
 
 def files_to_process(files_list: list, output_dir: str,
@@ -26,13 +27,13 @@ def files_to_process(files_list: list, output_dir: str,
     """
     Build a list of remaining files to process.
 
-    This function checks the data set integrity.
+    This function checks the data sets integrity.
 
     :param files_list: list
         The original list of file names to process.
         Only names is considered by default (is_base_name=True).
     :param output_dir: str
-        The path of the data set (output directory).
+        The path of the datasets set (output directory).
     :param contains_extension: bool
         Must be True if the file names contains file extensions (for instance
         file.wav). Default to True.
@@ -78,15 +79,15 @@ def create_dataset(dataset_dir: str, file_list: list, num_workers: int=None,
     Creates a dataset.
 
     :param dataset_dir: str
-        Output directory (data set).
+        Output directory (datasets set).
     :param file_list: list
         List of files to process (paths).
     :param num_workers: int
         The maximum number of processes that can be used to execute the given
         calls
     :param pre_processing: callable
-        Pre process data before saving. Default to None, no pre processing will
-        be performed.
+        Pre process datasets before saving. Default to None, no pre processing
+        will be performed.
     :param kwargs:
         Additional kwargs are passed on to the pre processing function.
     """
@@ -96,6 +97,11 @@ def create_dataset(dataset_dir: str, file_list: list, num_workers: int=None,
 
     # Set handler function to process each file
     handler = pre_processing if pre_processing is not None else copyfile
+
+    if num_workers == 1:
+        for file_path in file_list:
+            handler(file_path, dataset_dir, **kwargs)
+        return
 
     # Process data in parallel
     with concurrent.futures.ProcessPoolExecutor(max_workers=num_workers) as \
@@ -117,14 +123,14 @@ def create_dataset(dataset_dir: str, file_list: list, num_workers: int=None,
             for f in futures:
                 if f.exception() is not None:
                     log.write('\n' + str(f.exception()) +
-                              ' \n\twhen processing ' + f)
+                              ' \n\twhen processing ' + str(f))
 
 
 def pre_process(file_path: str, output_dir: str, name: str=None,
-                trim_interval: tuple=None, normalize=True,
-                verbose_level=0):
+                min_length: int=0, trim_interval: tuple=None, normalize=True,
+                rate: int=None, extra_sox_args: str=None, verbose_level=0):
     """
-    Pre process a file. Use this function to handle raw data.
+    Pre process a file. Use this function to handle raw datasets.
 
     :param file_path: str
         Path of the file to process.
@@ -132,34 +138,100 @@ def pre_process(file_path: str, output_dir: str, name: str=None,
         Path to save the processed file.
     :param name:
         Output name. Default to None (preserves the original name).
+    :param min_length: int
+        Minimum length of audio to consider. Skip otherwise.
+        Note: requires soxi.
     :param trim_interval: tuple, shape=(start, end)
         Trim audio. Default to None (no trim will be performed).
     :param normalize: bool
         Normalizes audio.
+    :param rate: int
+        Sets the rate of output file. Default to None (no conversion will be
+        performed).
+    :param extra_sox_args: str
+        Extra arguments are passed on as command line arguments.
     :param verbose_level: int
         Verbosity level. See sox for more information.
     """
+    if min_length > 0:
+        process = subprocess.Popen('soxi -D ' + file_path,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT)
+        stdout, stderr = process.communicate()
+        audio_length = float(stdout.decode(sys.stdout.encoding))
+        expected_length = trim_interval[1]
+        # print('->', audio_length, expected_length, min_length)
+        if audio_length < min_length or (trim_interval is not None and
+                                         trim_interval[0] +
+                                         expected_length > audio_length):
+            # Return if the audio length is less than the minimum length
+            # required or if the interval provided is not in the necessary range
+            # to make a trim.
+            return
     cmd = 'sox -V' + str(verbose_level) + ' ' + file_path
-    if name is not None:
-        cmd += ' ' + output_dir + os.sep + name + '.wav '
-    else:
-        cmd += ' ' + output_dir + os.sep + \
-               file_path.split(os.sep)[-1].split('.')[-2] + '.wav'
+    if name is None:
+        name = file_path.split(os.sep)[-1].split('.')[-2] + '_' + \
+               str(min_length)
+        if trim_interval is not None:
+            name += '_' + str(trim_interval[0]) + '_' + str(trim_interval[1])
+        if normalize:
+            name += '_norm'
+    cmd += ' ' + output_dir + os.sep + name + '.wav '
     if trim_interval is not None:
         cmd += ' trim ' + str(trim_interval[0]) + ' ' + str(trim_interval[1])
-    if normalize:
-        cmd += ' gain −n'
-    # print(cmd)
+    if extra_sox_args is not None:
+        cmd += extra_sox_args
+    if str(verbose_level) == '2':
+        print(cmd)
     os.system(cmd)
+    if normalize:
+        sound = AudioSegment.from_file(output_dir + os.sep + name + '.wav',
+                                       "wav")
+        change_in_d_bfs = (-20.0) - sound.dBFS
+        sound = sound.apply_gain(change_in_d_bfs)
+        sound.export(output_dir + os.sep + name + '.wav', format="wav")
+    if rate is not None:
+        # Get the current rate
+        process = subprocess.Popen('soxi -r ' + file_path,
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.STDOUT)
+        stdout, stderr = process.communicate()
+        current_rate = float(stdout.decode(sys.stdout.encoding))
+        # Convert the sample rate
+        cmd = 'sox -V' + str(verbose_level) + ' −r 16k ' + output_dir + \
+              os.sep + name + '.wav ' + output_dir + os.sep + name + '_' + \
+              str(rate) + '_temp.wav'
+        if str(verbose_level) == '2':
+            print(cmd)
+        os.system(cmd)
+        # Remove old file
+        os.remove(output_dir + os.sep + name + '.wav')
+        # Speed up to the original time
+        input_file = output_dir + os.sep + name + '_' + str(rate) + '_temp.wav'
+        cmd = 'sox -V' + str(verbose_level) + ' ' + input_file + ' ' + \
+              output_dir + os.sep + name + '_' + str(rate) + \
+              '.wav speed ' + str((current_rate/float(rate)))
+        if str(verbose_level) == '2':
+            print(cmd)
+        os.system(cmd)
+        # Remove old file
+        os.remove(input_file)
 
 
 if __name__ == '__main__':
+    import glob
+    import json
+    import argparse
+    import importlib
+    from collections import defaultdict
     # Command line arguments:
     parser = argparse.ArgumentParser(description='Generates a dataset of audio '
                                                  'files in a proper format.')
     parser.add_argument('corpus', help='Corpus information (JSON file)')
     parser.add_argument('output', help='Output directory.')
-    parser.add_argument('--seconds', help='Length of audio files in seconds.')
+    parser.add_argument('--seconds', help='Length of audio files in seconds.',
+                        type=int)
+    parser.add_argument('--rate', help='Set the output rate of audio files.')
     parser.add_argument('--workers', help='Define how many process to run in '
                                           'parallel.', default=4, type=int)
     parser.add_argument('--check', help='Check output directories ignoring '
@@ -171,14 +243,20 @@ if __name__ == '__main__':
                                              ' process. No processing will be '
                                              'performed.',
                         action='store_true')
+    parser.add_argument('--augment_data', help='Try to augment data by '
+                                               'trimming audio files in '
+                                               'different positions.',
+                        action='store_true')
     parser.add_argument('--v', help='Change verbosity level (sox output)',
                         default=0)
 
     # Set arguments settings:
     arguments = parser.parse_args()
-    data_dir = arguments.source
+    data_dir = arguments.corpus
     output = arguments.output
     seconds = arguments.seconds
+    augment_data = arguments.augment_data
+    output_rate = arguments.rate
     workers = arguments.workers
     verbose = arguments.v
 
@@ -247,8 +325,7 @@ if __name__ == '__main__':
 
         d = dict()
         d['New base'] = list(files_list_lang.keys())
-        d['Total samples'] = list(map(lambda x:
-                                      len(files_list_lang[x]),
+        d['Total samples'] = list(map(lambda x: len(files_list_lang[x]),
                                       files_list_lang))
         print('\nSUMMARY\n-------')
         print(str(pd.DataFrame(d)) + '\n')
@@ -262,10 +339,20 @@ if __name__ == '__main__':
             continue
         print('[INFO] processing base "%s"' % base)
         # Call function and make data set
-        create_dataset(dataset_dir=output + os.sep + base,
-                       file_list=files_list_lang[base],
-                       num_workers=workers,
-                       pre_processing=pre_process,
-                       verbose_level=verbose,
-                       trim_interval=(0, seconds) if seconds is not None else
-                       None)
+        # Create attempts to trim in different positions. A better approach to
+        # this would be making the trims considering the audio length, but this
+        # approach is good for now.
+        if augment_data and seconds is not None:
+            trims = 4
+        else:
+            trims = 1
+        for d in range(0, trims, 2):
+            create_dataset(dataset_dir=output + os.sep + base,
+                           file_list=files_list_lang[base],
+                           num_workers=workers,
+                           pre_processing=pre_process,
+                           verbose_level=verbose,
+                           min_length=seconds if seconds is not None else 0,
+                           rate=output_rate,
+                           trim_interval=(0 + d, seconds)
+                           if seconds is not None else None)
