65f5c70ee03b347446a55aadcb185cd7cea26899
lucasrgris <lucasrgris@gmail.com>
2018-11-06 14:22:27 -0200

    Add exception handling in case the file is not found

--- util/datasets/numpyloader.py
+++ util/datasets/numpyloader.py
@@ -5,7 +5,8 @@ from util.datasets.csv import CSVParser as Parser
 import numpy as np
 
 
-def load_dataset(csv_dataset_path: str, expected_shape: tuple=None):
+def load_dataset(csv_dataset_path: str, expected_shape: tuple=None,
+                 verbose=False, not_found_ok=False):
     """
     Loads a dataset saved in numpy binary files (.npy format).
 
@@ -21,14 +22,33 @@ def load_dataset(csv_dataset_path: str, expected_shape: tuple=None):
         Check if the shape of each loaded data is in the provided format. If
         not, the data will be ignored.
 
+    :param verbose: bool
+        Enable/Disable verbose messages (progress).
+
+    :param not_found_ok: bool
+        If false, will raise a FileNotFoundError, if true, will ignore the file.
+        Default to false.
+
     :return: tuple (numpy.ndarray, numpy.ndarray)
         A tuple with the data loaded and the respective labels.
     """
     paths, labels = Parser(csv_dataset_path)()
     X = []
     y = []
+    i = 0
     for path, label in zip(paths, labels):
-        data = np.load(path)
+        i += 1
+        if verbose and i % 1000 == 0 and i > 1:
+            print('[INFO] data: {}/{}'.format(i, len(paths)))
+
+        if not_found_ok:
+            try:
+                data = np.load(path)
+            except FileNotFoundError:
+                continue
+        else:
+            data = np.load(path)
+
         if expected_shape is not None and data.shape != expected_shape:
             continue
         X.append(data)
