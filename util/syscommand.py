0a4979ba8924f0a7150b249fb142a7296e1973b4
lucasrgris <lucasrgris@gmail.com>
2019-04-04 08:51:47 -0300

    Add debug option for syscommand

--- util/syscommand.py
+++ util/syscommand.py
@@ -2,9 +2,21 @@ import subprocess
 import sys
 
 
-def system(command: str) -> str:
+def system(command: str, debug: bool=False):
+    """
+    Runs a system command
+
+    debug: bool
+        If True, will print the command.
+    return_out: bool
+    """
+    if debug:
+        print(command)
     process = subprocess.Popen(command,
                                stdout=subprocess.PIPE,
-                               stderr=subprocess.STDOUT)
+                               stderr=subprocess.STDOUT,
+                               shell=True)
     stdout, stderr = process.communicate()
+    if debug and stderr is not None:
+        print(stderr.decode(sys.stdout.encoding))
     return stdout.decode(sys.stdout.encoding)
